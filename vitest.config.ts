e315e1d8a77a41cb6b58b29f562fb913e53496b1
Joe Moran <joemoran231@gmail.com>
2024-04-19 13:23:54 -0400

    chore: introduce testing for react components

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,11 @@
 import { getViteConfig } from 'astro/config';
+import react from '@vitejs/plugin-react';
 export default getViteConfig({
-   
-  });
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.test.jsx', 'src/**/*.test.js', 'src/**/*.test.tsx'],
+    setupFiles: ['./setupTest.js'],
+  },
+});
